dc70b0b550cfc91b7b0bbe0b27cdce0a14a30e27
Baba0608 <babafakruddinnitturu@gmail.com>
2024-10-14 21:07:21 +0530

    Variables and DataTypes

---
+++ 1.Basics/Variables-and-datatypes.js
@@ -0,0 +1,31 @@
+// variables
+
+// We can define variables using var, let, const.
+
+var a = 10; // stores 10 in variable 10.
+
+let b = 20; // stores integer 20 in variable b.
+
+const c = true; // stores boolean true in variable in c.
+
+// Most used operators
+/**
+ *  +  => addition
+ *  -  => subtraction
+ *  *  => multiplication
+ *  /  => division
+ *  %  => to get remainder
+ * 
+ *  && => and
+ *  || => or
+ *  ^  => XOR
+ */
+
+// Data Types
+/**
+ * Integer => 10
+ * Float   => 10.0
+ * String  => "JavaScript"
+ * Boolean => true
+ * Array   => [10 , 20.4 , 30 , "JS" , true]
+ */
