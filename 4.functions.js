fab2a5e7dc54d38d7ec271c6c417b68892557920
Baba0608 <babafakruddinnitturu@gmail.com>
2024-11-06 19:11:51 +0530

    functions basics

---
+++ 4.functions.js
@@ -0,0 +1,24 @@
+// functions are used to reuse the code, to make the code readable etc.
+
+// We define a function using the keyword function
+// Ex:
+// declaring the function
+function sum(a, b) {
+  // logic
+  let c = a + b;
+  console.log(c);
+}
+
+// calling the function
+sum(10, 20);
+
+// In the above exampe `sum` is the function name (which we will use to call the function later)
+// a , b are the parameters of that function.
+// 10 , 20 are arguments.
+
+// While defining the function we use the term parameters(a , b). But while calling the function they are known as arguments(10 , 20  {the actual values}).
+
+// We define the function once, and can call the function multiple times with different arguments.
+sum(100, 200);
+sum(50, 80);
+sum(19, 23);
